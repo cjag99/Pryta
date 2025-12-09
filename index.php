ec0342c80900bc062e0522a75664660151325510
cjag99 <cjag99@gmail.com>
2025-12-09 11:47:34 +0100

    Refactor structure, styles and login form

--- index.php
+++ index.php
@@ -5,19 +5,72 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log in | Pryta Tech</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="src/styles/style.css">
+    <link rel="shortcut icon" href="./src/images/logo.ico" type="image/x-icon">
 </head>
 <body>
     <div class="loginWindow">
         <div class="container">
-            <div class="row row-cols-2">
-                <div class="col-2 col-md-1">
-                    <div class="container-fluid">
-                        <h3>Welcome to Pryta Tech</h3>
+            <div class="row">
+                <div class="col-6 text-light container-fluid">
+                    <br><br>
+                    <div class="d-flex flex-row align-items-center">
+                        <div>
+
+                           <img src="./src/images/logo.svg" alt="Pryta Logo">
+                        </div>
+                        <div>
+                            <h4 class="text-light m-0">Pryta</h4>
+                        </div>
                     </div>
+                    <form action="./authentication.php" method="post">
+                        <h3>Welcome to Pryta Tech</h3>
+                        <div class="mb-3">
+                            <label for="user" class="form-label text-light">Please enter yoor username:</label> <br>
+                            <input
+                                type="text"
+                                class="form-control"
+                                name="user"
+                                id="user"
+                                aria-describedby="helpId"
+                                placeholder="User must not be empty"
+                                required
+                            />
+                            <small id="helpId" class="form-text text-muted">Help text</small>
+                        </div>
+                        <div class="mb-3">
+                            <label for="passwd" class="form-label text-light">Please enter your password:</label> <br>
+                            <input
+                                type="text"
+                                class="form-control"
+                                name="passwd"
+                                id="passwd"
+                                aria-describedby="helpId"
+                                placeholder="Password must be between 8-16 characters"
+                                required
+
+                            />
+                            <small id="helpId" class="form-text text-muted">Help text</small>
+                        </div>
+                        <button
+                            type="submit"
+                            class="btn btn-primary"
+                        >
+                            Login
+                        </button>
+                        <button
+                            type="reset"
+                            class="btn btn-outline-light"
+                        >
+                            Reset
+                        </button>
+                        
+                    </form>
                 </div>
-                <dv class="col-md-1">
-                    <img src="./ejemplo.jpg" alt="" >
-                </dv>
+
+                <div class="col-6">
+                    <img src="./src/images/ejemplo.jpg" alt="">
+                </div>
             </div>
         </div>
     </div>
